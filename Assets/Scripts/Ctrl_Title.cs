44e75c0e4227705eefbea160ffd127bb7c8aab87
Seok-Min-Lee <57294634+Seok-Min-Lee@users.noreply.github.com>
2025-09-01 22:06:29 +0900

    비밀번호 입력 작업
    UI 및 키보드

--- Assets/Scripts/Ctrl_Title.cs
+++ Assets/Scripts/Ctrl_Title.cs
@@ -1,28 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ctrl_Title : MonoBehaviour
 {
+    [SerializeField] private PasswordDigit[] digits;
+    private int cursorIndex = 0;
+
     private void Start()
     {
         Debug.Log("Client is Available? " + Client.Instance == null);
+
+        digits[0].SetFocus();
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        for (int i = 0; i <= 9; i++)
         {
-            Client.Instance.RequestCheckPassword(2848);
+            if (Input.GetKeyDown(KeyCode.Alpha0 + i) ||
+                Input.GetKeyDown(KeyCode.Keypad0 + i))
+            {
+                OnClickDigitButton(i);
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Client.Instance.RequestCheckPassword(9999);
-        }
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter)) OnClickSubmit();
+        if (Input.GetKeyDown(KeyCode.Backspace)) OnClickRemove();
     }
 
     public void OnClickDigitButton(int num)
     {
+        if (cursorIndex == digits.Length) 
+        { 
+            return;
+        }
 
+        digits[cursorIndex++].SetText(num + ""); 
+
+        if (cursorIndex < digits.Length) 
+        {
+            digits[cursorIndex].SetFocus(); 
+        }
+    }
+    public void OnClickRemove()
+    {
+        if (cursorIndex == 0)
+        {
+            return;
+        }
+
+        if (cursorIndex < digits.Length)
+        {
+            digits[cursorIndex].SetText("");
+        }
+
+        digits[--cursorIndex].SetFocus();
+    }
+    public void OnClickSubmit()
+    {
+        string pwStr = string.Empty;
+
+        for (int i = 0; i < digits.Length; i++)
+        {
+            pwStr += digits[i].Value;
+        }
+
+        if (int.TryParse(pwStr, out int pw))
+        {
+            Client.Instance.RequestCheckPassword(pw);
+        }
+        else
+        {
+            Debug.Log("Fail :: " + pwStr);
+        }
     }
 }
---
+++ Assets/Scripts/PasswordDigit.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PasswordDigit : MonoBehaviour
+{
+    public string Value
+    { 
+        get 
+        {
+            return text.text;
+        } 
+    }
+    [SerializeField] private TextMeshProUGUI text;
+
+    private Animation animation;
+    private CanvasGroup canvasGroup;
+    private void Start()
+    {
+        text.text = string.Empty;
+        animation = text.GetComponent<Animation>();
+        canvasGroup = text.GetComponent<CanvasGroup>();
+    }
+    public void SetText(string text)
+    {
+        this.text.text = text;
+        animation.Stop();
+        canvasGroup.alpha = 1f;
+    }
+    public void SetFocus()
+    {
+        this.text.text = "_";
+        animation.Play();
+    }
+}
